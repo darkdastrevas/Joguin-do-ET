3b8df14f098c83e2408793d0b5bbaed15b9aa1d5
Soneca-tws <pedro.otavio.cg@hotmail.com>
2024-11-27 01:24:01 -0300

    Fade e respawn do bogos
    está faltando a camera ir para o bogos quando ele dá respawn e está faltando você controlar o bogos quando renasce

--- Assets/Scripts/HeartSystem.cs
+++ Assets/Scripts/HeartSystem.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class HeartSystem : MonoBehaviour
 {
-    // VARI�VEIS
+    // VARIÁVEIS
     public int vidaMax;
     public int vidaAtual;
 
@@ -13,18 +14,32 @@ public class HeartSystem : MonoBehaviour
 
     private Animator anim;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Image fadeImage; // Imagem para o fade in/out
+    [SerializeField] private float fadeDuration = 1.0f; // Duração do fade
+    [SerializeField] private Transform startPoint; // Ponto inicial para resetar o personagem
+
+    private bool isFading = false;
+
     void Start()
     {
         vidaAtual = vidaMax;
+        if (fadeImage != null)
+        {
+            fadeImage.color = new Color(0, 0, 0, 0); // Começa transparente
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         HealthLogic();
+
+        if (vidaAtual <= 0 && !isFading) // Quando a vida chega a 0
+        {
+            StartCoroutine(HandleDeath());
+        }
     }
-    // L�GICA DA VIDA
+
+    // LÓGICA DA VIDA
     void HealthLogic()
     {
         for (int i = 0; i < heart.Length; i++)
@@ -48,7 +63,40 @@ public class HeartSystem : MonoBehaviour
                 heart[i].enabled = false;
             }
         }
-
     }
 
+    // COROUTINE PARA FADE E RESET
+    private IEnumerator HandleDeath()
+    {
+        isFading = true;
+
+        // Fade Out (para preto)
+        yield return StartCoroutine(Fade(1));
+
+        // Resetar posição e vida
+        transform.position = startPoint.position;
+        vidaAtual = vidaMax;
+
+        // Fade In (de volta à cena)
+        yield return StartCoroutine(Fade(0));
+
+        isFading = false;
+    }
+
+    // Função de fade genérica
+    private IEnumerator Fade(float targetAlpha)
+    {
+        float startAlpha = fadeImage.color.a;
+        float elapsedTime = 0;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeDuration);
+            fadeImage.color = new Color(0, 0, 0, newAlpha);
+            yield return null;
+        }
+
+        fadeImage.color = new Color(0, 0, 0, targetAlpha);
+    }
 }
